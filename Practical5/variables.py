42305dc153cd588d74a23adb0486bdb5c3a6d90b
skyssssss <3190110632@zju.edu.cn>
2020-08-14 00:47:27 +0800

    Add variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 14 00:31:43 2020
+
+@author: skysblues
+"""
+a=5673
+b=151100
+c=a*3
+d=b/10
+if c == d:
+    print("c is equal to d")
+elif c < d:
+    print("d is bigger than c")
+else:
+    print("c is bigger than d")
